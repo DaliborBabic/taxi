07c1bc73822db57f12e788f5e4ce67541b1e080e
DaliborBabic <dalibor.babic@student.ukf.sk>
2017-05-01 22:23:46 +0200

    Hlavička,päta

---
+++ application/views/template/footer.php
@@ -0,0 +1,13 @@
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+</body>
+</html><?php
+/**
+ * Created by PhpStorm.
+ * User: NAY
+ * Date: 25. 4. 2017
+ * Time: 8:54
+ */
