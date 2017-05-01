c30dddeb04231e9b248357d135c284ee8c7eb092
DaliborBabic <dalibor.babic@student.ukf.sk>
2017-05-01 22:27:02 +0200

    Hlavička,päta

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -4,7 +4,7 @@
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
 </body>
-</html><?php
+</html>
 /**
  * Created by PhpStorm.
  * User: NAY
