b23686b5f978ba9faaf2a0dd260661e691fa70c1
DaliborBabic <dalibor.babic@student.ukf.sk>
2017-05-02 07:51:58 +0200

    autoload

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,8 +1,9 @@
+<?php
 public function index()
 
 { $this->load->view('template/header');
 $this->load->view('about');
-$this->load->view('template/footer'); }<?php
+$this->load->view('template/footer'); }
 /**
  * Created by PhpStorm.
  * User: NAY
